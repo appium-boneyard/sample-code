89b3bfd5e340b12e1003e1afc22195e2d71d3e84
michaelbredman <mredman@gmail.com>
2014-09-10 21:41:38 -0700

    removing files

--- sample-code/examples/python/sub.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-p1 = subprocess.Popen(['./sc/bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -i AppiumTest'],
-                      shell=True, stdout=subprocess.PIPE)
-
-print p1.stdout.readlines()
