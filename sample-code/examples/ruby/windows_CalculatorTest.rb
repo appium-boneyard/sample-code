836f025dfcf114d4fdf26261b7cf003000a79b2a
alakshmi1030 <anlak@microsoft.com>
2016-08-12 15:42:13 -0700

    Added copyright in Java and Ruby test

--- sample-code/examples/ruby/windows_CalculatorTest.rb
+++ sample-code/examples/ruby/windows_CalculatorTest.rb
@@ -1,5 +1,7 @@
 # ******************************************************************************
 #
+# Copyright (c) 2016 Appium Committers. All rights reserved.
+#
 #Licensed to you under the Apache License, Version 2.0 (the
 #"License"); you may not use this file except in compliance
 #with the License.  You may obtain a copy of the License at
