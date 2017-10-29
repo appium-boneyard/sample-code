7224af003378388a9e0341f826f920f1d28b4589
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-10-29 18:05:49 +0900

    remove unused files

--- sample-code/apps/TestApp/build/TestApp.build/Release-iphonesimulator/TestApp.build/Objects-normal/i386/MyViewControllerViewController.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/kazuaki/GitHub/ios-test-app/Test\ App\ 2/TestApp-Prefix.pch \
-  /Users/kazuaki/GitHub/ios-test-app/Test\ App\ 2/MyViewControllerViewController.m \
-  /Users/kazuaki/GitHub/ios-test-app/Test\ App\ 2/MyViewControllerViewController.h \
-  /Users/kazuaki/GitHub/ios-test-app/Test\ App\ 2/GestureTestViewController.h
--- sample-code/apps/TestApp/build/TestApp.build/Release-iphonesimulator/TestApp.build/Objects-normal/i386/TA2AppDelegate.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/kazuaki/GitHub/ios-test-app/Test\ App\ 2/TestApp-Prefix.pch \
-  /Users/kazuaki/GitHub/ios-test-app/Test\ App\ 2/TA2AppDelegate.m \
-  /Users/kazuaki/GitHub/ios-test-app/Test\ App\ 2/TA2AppDelegate.h \
-  /Users/kazuaki/GitHub/ios-test-app/Test\ App\ 2/MyViewControllerViewController.h \
-  /Users/kazuaki/GitHub/ios-test-app/Test\ App\ 2/GestureTestViewController.h
